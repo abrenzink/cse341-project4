51c5c9c2ded5e5a1f4ee65d59a8eebfdd915b1c4
abrenzink <a.brenzink@gmail.com>
2023-10-04 19:45:40 -0300

    express session initialization on server.js

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ const dotenv = require('dotenv');
 const mongodb = require('./data/database');
 const bodyParser = require('body-parser');
 const { logError, returnError } = require('./middleware/errorHandler');
+const GitHubStrategy = require('passport-github2').Strategy;
+const passport = require('passport');
+const cors = require('cors');
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT;
 
 app.use(bodyParser.json())
+// basic express session initialization
+    .use(session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+    }))
+    // init passport on every route call
+    .use(passport.initialize())
+    // allow passport to use "express-session"
+    .use(passport.session())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
