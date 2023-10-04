7f713cea338e8d673a4fbdd08e95b23ab0b3c82d
abrenzink <a.brenzink@gmail.com>
2023-10-04 20:36:27 -0300

    login and logout routes

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,15 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 router.use('/words', require('./words'));
 
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next){
+    req.logout(function(err){
+        if (err){
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
