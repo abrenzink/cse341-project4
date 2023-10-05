a9838bc2dbac8484d1bea3258b0c85efbea44ac5
abrenzink <a.brenzink@gmail.com>
2023-10-05 15:42:04 -0300

    corrections on login route

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 router.use('/', require('./swagger'));
 router.use('/words', require('./words'));
 
-router.get('/log', passport.authenticate('github'), (req, res) => {});
+router.get('/login', passport.authenticate('github'), (req, res) => {});
 
 router.get('/logout', function(req, res, next){
     req.logout(function(err){
